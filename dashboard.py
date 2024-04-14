3937b60d74677982ae906fa95b0966d58d0e0ecd
mayur-tarwade <tarwade.mayur@gmail.com>
2024-04-14 19:00:50 +0530

    graphs added

--- dashboard.py
+++ dashboard.py
@@ -9,10 +9,32 @@ app = dash.Dash()
 
 app.layout = html.Div([
     html.Div(children=[
-        html.H1('First Dashboard', style={'color':'orange', 'text-align':'center','margin':'5px','padding':'5px'})
+        html.H1('First Dashboard', style={'color':'blue', 'text-align':'center','margin':'5px','padding':'5px'})
     ],style={'border':'1px black solid', 'float':'left','width':'100%', 'height':'50px'}),
-    html.Div(style={'border':'1px black solid', 'float':'left','width':'50%', 'height':'350px'}),
-    html.Div(style={'border':'1px black solid', 'float':'left','width':'49.70%', 'height':'350px'}),
+    html.Div(children=[
+        dcc.Graph(id='Scatter Plot',
+                  figure={'data':[go.Scatter(x=data['pop'],
+                                             y=data['gdpPercap'],
+                                             mode='markers')],
+                          'layout':go.Layout(title='Scatter Plot',
+                                             xaxis={'title':'Pop'},
+                                             yaxis={'title':'gdp/cap'})})
+    ],style={'border':'1px black solid', 'float':'left','width':'50%', 'height':'490px'}),
+    html.Div(children=[
+        dcc.Graph(id='Bar Plot',
+                  figure={'data':[go.Bar(x=data['year'],
+                                         y=data['pop'],
+                                         marker={'color':'blue'},
+                                         name='country')],
+                          'layout':go.Layout(title='Bar Plot',
+                                             xaxis={'title':'Year'},
+                                             yaxis={'title':'Pop'})})
+    ],style={'border':'1px black solid', 'float':'left','width':'49.70%', 'height':'490px'}),
+html.Div(children=[
+        dcc.Graph(id='Box Plot',
+                  figure={'data':[go.Box(x=data['gdpPercap'])],
+                          'layout':go.Layout(title='Box Plot')})
+    ],style={'border':'1px black solid', 'float':'left','width':'50%', 'height':'490px'}),
 ])
 
 if __name__== '__main__':
